6f3e579a18f49be0718a54c3f9dd9d7b762cabb4
Camilo <villaloboscamilo571@gmail.com>
2025-05-14 10:16:33 -0600

    un pequeño cambio en init.py de compras

--- app/blueprints/compras/__init__.py
+++ app/blueprints/compras/__init__.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
 
-compras_bp = Blueprint('compras', __name__, url_prefix='', 
-                        template_folder='templates')
+compras_bp = Blueprint('compras', __name__,
+                        url_prefix='/compras', 
+                        template_folder='templates',
+                        static_folder='static',)
 
 from app.blueprints.compras.routes import *
